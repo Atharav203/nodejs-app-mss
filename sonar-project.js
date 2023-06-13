1e98c9e9628eefa9cd050c8bc4d49a7ae2c2341f
Priyanka60486 <130199576+Priyanka60486@users.noreply.github.com>
2023-06-13 16:55:55 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,13 +1,13 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: http://18.183.168.23:9000/,
+  serverUrl: 'http://18.183.168.23:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
 	    'sonar.login': 'sqa_dcbf9aede98f0e5bcf3774db37f91d155caecea5',
-	    //'sonar.login': 'priyanka',
+	    //'sonar.login': 'admin',
 	    //'sonar.password': 'password',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
